dc2d899194bd35714088b46f70cc1b4f546692db
Korver <korver2017@gmail.com>
2022-07-28 14:09:00 +0800

    feat: Import styles and fonts

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 
+// Nucleo Icons
+import '@/assets/css/nucleo-icons.css';
+import '@/assets/css/nucleo-svg.css';
+import '@/assets/css/nucleo-svg.css';
+// CSS Styles
 import '@/assets/scss/soft-design-system-pro.scss';
 
 const app = createApp(App);
