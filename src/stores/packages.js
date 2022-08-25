78d04804a3908727de9a361f4594159e215efc0a
Korver <korver2017@gmail.com>
2022-08-25 13:14:38 +0800

    feat: Add loading effect to functions

--- src/stores/packages.js
+++ src/stores/packages.js
@@ -3,14 +3,17 @@ import { reactive } from 'vue';
 import { useRoute } from 'vue-router';
 import { apiGetPackages } from '@/api/packagesLoader';
 import { apiGetPackage } from '@/api/packagesLoader';
+import { mountLoading, unmountLoading } from '@/composition-api/useLoading';
 import { packages as menu } from '@/const/menus';
 
 export const usePackageStore = defineStore('packages', () => {
   const packages = reactive({ categories: {} });
 
   const getPackages = async () => {
+    mountLoading();
     const result = await apiGetPackages();
     packages.categories = result.data;
+    unmountLoading();
   };
 
   const menuItems = menu.map((item) => item.name);
@@ -21,13 +24,14 @@ export const usePackageStore = defineStore('packages', () => {
   // Watch for router changes and update category data.
   const watchCategoryChanged = () =>
     watchEffect(async () => {
-      category.data = {};
-
       // When leaving the page or route is not match to any menu item, return.
       if (menuItems.indexOf(source.value) < 0) return;
+      mountLoading();
+      category.data = {};
 
       const result = await apiGetPackage(source.value);
       category.data = result.data;
+      unmountLoading();
     });
 
   return { getPackages, packages, watchCategoryChanged, category };
--- src/stores/tools.js
+++ src/stores/tools.js
@@ -3,14 +3,17 @@ import { computed, reactive } from 'vue';
 import { useRoute } from 'vue-router';
 import { apiGetTools } from '@/api/toolsLoader';
 import { apiGetTool } from '@/api/toolsLoader';
+import { mountLoading, unmountLoading } from '@/composition-api/useLoading';
 import { tools as menu } from '@/const/menus';
 
 export const useToolStore = defineStore('tools', () => {
   const tools = reactive({ categories: {} });
 
   const getTools = async () => {
+    mountLoading();
     const result = await apiGetTools();
     tools.categories = result.data;
+    unmountLoading();
   };
 
   const menuItems = menu.map((item) => item.name);
@@ -21,13 +24,14 @@ export const useToolStore = defineStore('tools', () => {
   // Watch for router changes and update category data.
   const watchCategoryChanged = () =>
     watchEffect(async () => {
-      category.data = {};
-
       // When leaving the page or route is not match to any menu item, return.
       if (menuItems.indexOf(tool.value) < 0) return;
+      mountLoading();
+      category.data = {};
 
       const result = await apiGetTool(tool.value);
       category.data = result.data;
+      unmountLoading();
     });
 
   return { getTools, tools, watchCategoryChanged, category };
