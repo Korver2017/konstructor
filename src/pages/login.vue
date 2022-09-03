2f573140416b47eea803b22df8174e3fb396a89c
Korver <korver2017@gmail.com>
2022-09-03 14:51:14 +0800

    refactor: Re-new login component

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,77 +1,68 @@
 <template>
-  <div class="sign-in-cover">
-    <section>
-      <div class="page-header min-vh-75">
-        <div class="container">
-          <div class="row">
-            <div class="col-xl-4 col-lg-5 col-md-6 d-flex flex-column mx-auto">
-              <div class="card card-plain mt-8">
-                <div class="card-header pb-0 text-left">
-                  <h3 class="font-weight-bolder text-info text-gradient">
-                    Welcome back
-                  </h3>
-                  <p class="mb-0">Enter your email and password to sign in</p>
-                </div>
-                <div class="card-body">
-                  <form>
-                    <label>Account</label>
-                    <div class="mb-3">
-                      <input
-                        type="email"
-                        class="form-control"
-                        placeholder="Email"
-                        aria-label="Email"
-                        v-model="userInputs.account"
-                        @keyup.enter="login"
-                      />
-                    </div>
-                    <label>Password</label>
-                    <div class="mb-3">
-                      <input
-                        type="email"
-                        class="form-control"
-                        placeholder="Password"
-                        aria-label="Password"
-                        v-model="userInputs.password"
-                        @keyup.enter="login"
-                      />
-                    </div>
-                    <div class="text-center">
-                      <button
-                        type="button"
-                        class="btn bg-gradient-info w-100 mt-4 mb-0"
-                        @click="login(userInputs)"
-                      >
-                        Sign in
-                      </button>
-                    </div>
-                  </form>
-                </div>
+  <section>
+    <div class="page-header min-vh-100">
+      <span class="mask bg-gradient-secondary opacity-5"></span>
+      <div class="container">
+        <div class="row">
+          <div class="col-xl-4 col-lg-5 col-md-7 mx-auto">
+            <div class="card z-index-0">
+              <div class="card-header text-center pb-1">
+                <h4 class="font-weight-bolder mb-1">Sign in</h4>
+                <p class="mb-0">Welcome back</p>
               </div>
-            </div>
-            <div class="col-md-6">
-              <div
-                class="oblique position-absolute top-0 h-100 d-md-block d-none me-n8"
-              >
-                <div
-                  class="oblique-image bg-cover position-absolute fixed-top ms-auto h-100 z-index-0 ms-n6"
-                  :style="{
-                    backgroundImage: `url(${sideImage})`,
-                  }"
-                ></div>
+              <div class="card-body">
+                <form>
+                  <div class="mb-3">
+                    <input
+                      v-model="userInputs.account"
+                      type="email"
+                      class="form-control"
+                      placeholder="Email"
+                      aria-label="Email"
+                    />
+                  </div>
+                  <div class="mb-3">
+                    <input
+                      v-model="userInputs.password"
+                      type="password"
+                      class="form-control"
+                      placeholder="Password"
+                      aria-label="Password"
+                    />
+                  </div>
+                  <div class="text-center">
+                    <button
+                      @click="login"
+                      type="button"
+                      class="btn bg-gradient-info w-100 mt-4 mb-2"
+                    >
+                      Sign in
+                    </button>
+                  </div>
+                </form>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </section>
-  </div>
+    </div>
+  </section>
 </template>
 
 <script setup>
-  import sideImage from '@/assets/img/curved-images/curved9.jpg';
   import { useUserStore } from '@/stores/users';
   const { userInputs, login } = useUserStore();
 </script>
 
-<style scoped></style>
+<style scoped>
+  .page-header {
+    background: url('~@/assets/img/curved-images/curved11.jpg');
+    background-size: cover;
+  }
+  .card-header {
+    padding-top: 50px;
+  }
+  .card-body {
+    padding-bottom: 35px;
+  }
+</style>
